a7dc635a85b2948301182c633834be7370ddfb05
Demian Holderegger <demian.holderegger@gmail.com>
2020-04-04 15:12:45 +0200

    add mapview

--- components/map/MapMain.tsx
+++ components/map/MapMain.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Text} from 'react-native';
+import MapView from 'react-native-maps';
 import styled from "styled-components/native";
+import {Dimensions} from "react-native";
 
 const MainContainer = styled.View`
 	flex: 1;
@@ -9,10 +10,25 @@ const MainContainer = styled.View`
 	justify-content: center;
 `;
 
+
 export default function MapMain() {
 	return (
 		<MainContainer>
-			<Text>Map</Text>
+			<MapView
+				style={{
+					width: Dimensions.get('window').width,
+					height: Dimensions.get('window').height,
+				}}
+				initialRegion={{
+					latitude: 46.5021646,
+					longitude: 8.2962415,
+					latitudeDelta: 0.065,
+					longitudeDelta: 0.04,
+				}}
+				minZoomLevel={8}
+				mapType="hybrid"
+				//provider="google"
+			/>
 		</MainContainer>
 	);
 }
