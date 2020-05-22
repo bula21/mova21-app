35cccc1032886de3e5b3da1204e47e31f3623214
Demian Holderegger <demian.holderegger@gmail.com>
2020-05-22 16:26:08 +0200

    add getColorByName function to theme

--- constants/MovaTheme.ts
+++ constants/MovaTheme.ts
@@ -2,6 +2,19 @@ export default {
 	colorYellow: '#FFEB00',
 	colorOrange: '#E6500F',
 	colorBlue: '#55BEFF',
-	colorGrey: '#737373',
+	colorGrey: '#0000008C',
 	colorBlack: '#000',
+	colorWhite: '#FFF',
+	// color helper
+	getColorByName: function (color: string) {
+		switch (color) {
+			case 'yellow': return this.colorYellow;
+			case 'orange': return this.colorOrange;
+			case 'blue': return this.colorBlue;
+			case 'grey': return this.colorGrey;
+			case 'black': return this.colorBlack;
+			case 'white': return this.colorWhite;
+			default: return this.colorWhite;
+		}
+	}
 }
