ebbb1f7d07d2614fc1a8e0954a8d79db44df4da0
Jonas Marty <jonasmarty@hotmail.com>
2020-11-29 12:25:08 +0100

    Translation is no working :tada:

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import "./src/i18n";
 //import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
