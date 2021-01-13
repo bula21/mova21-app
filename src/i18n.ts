66597aef5af2e6007e1026d5814aee447b5077ed
Demian Holderegger <demian.holderegger@gmail.com>
2021-01-13 20:35:05 +0100

    format files

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,5 @@
-import i18n from "i18next";
-import {initReactI18next} from "react-i18next";
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
 
 import translationDE from '../translations/de.json';
 import translationEN from '../translations/en.json';
@@ -8,39 +8,39 @@ import translationIT from '../translations/it.json';
 
 // the translations
 const resources = {
-	de: {
-		translation: translationDE
-	},
-	en: {
-		translation: translationEN
-	},
-	fr: {
-		translation: translationFR
-	},
-	it: {
-		translation: translationIT
-	}
+  de: {
+    translation: translationDE,
+  },
+  en: {
+    translation: translationEN,
+  },
+  fr: {
+    translation: translationFR,
+  },
+  it: {
+    translation: translationIT,
+  },
 };
 
-i18n
-	.use(initReactI18next)
-	.init({
-		resources,
-		lng: "de",
-		fallbackLng: "de",
-		react: {
-			defaultTransParent: 'div',
-			transSupportBasicHtmlNodes: true,
-			transKeepBasicHtmlNodesFor: ['br', 'strong', 'i'],
-		},
-		interpolation: {
-			escapeValue: false, // not needed for react!!
-			formatSeparator: ',',
-			format(value, format) {
-			  	if (format === 'uppercase') return value.toUpperCase();
-			  	return value;
-			},
-		},
-	});
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'de',
+  fallbackLng: 'de',
+  react: {
+    defaultTransParent: 'div',
+    transSupportBasicHtmlNodes: true,
+    transKeepBasicHtmlNodesFor: ['br', 'strong', 'i'],
+  },
+  interpolation: {
+    escapeValue: false, // not needed for react!!
+    formatSeparator: ',',
+    format(value, format) {
+      if (format === 'uppercase') {
+        return value.toUpperCase();
+      }
+      return value;
+    },
+  },
+});
 
 export default i18n;
