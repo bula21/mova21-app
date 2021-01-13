49f702599f45ca9f5b5295b78330786e644e3a64
Demian Holderegger <demian.holderegger@gmail.com>
2021-01-13 21:45:11 +0100

    change default backend url

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,7 +1,7 @@
 const appConfig: {
   backendUrl: string;
 } = {
-  //backendUrl: 'https://directus.bula21.ch',
-  backendUrl: 'http://backend.pio-x.ch:8885',
+  backendUrl: 'https://directus.bula21.ch',
+  //backendUrl: 'http://backend.pio-x.ch:8885',
 };
 export default appConfig;
