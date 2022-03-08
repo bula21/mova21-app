39dc2fe89380eb142594b77333c40c3c066beea5
Demian Holderegger <demian.holderegger@gmail.com>
2022-03-08 22:14:08 +0100

    fix language detection

--- src/helpers/LanguageManager.ts
+++ src/helpers/LanguageManager.ts
@@ -8,10 +8,10 @@ class LanguageManager {
   public onChange: Subject<string> = new Subject();
 
   public async changeLanguageTo(language: string): Promise<void> {
+    language = this.sanitizeLanguage(language);
     this._currentLanguage = language;
     await AsyncStorage.setItem('language', language);
     this.applyLanguage(language);
-    this.onChange.next(language);
     console.log('set language to ' + language);
   }
 
@@ -27,13 +27,28 @@ class LanguageManager {
 
   public async applyLanguageFromStorageOrDevice(): Promise<void> {
     let currentLanguage = await this.getCurrentLanguageAsync();
-    this.applyLanguage(currentLanguage);
+    this.applyLanguage(this.sanitizeLanguage(currentLanguage));
   }
 
   private applyLanguage(language: string): void {
-    i18n.changeLanguage(language.split('_')[0], () =>
+    this.onChange.next(language);
+    i18n.changeLanguage(language, () =>
       console.log('Changed language to ' + language),
     );
+  }
+
+  private sanitizeLanguage(language: string) {
+    if (!language) {
+      return 'de';
+    }
+    language = language.toLowerCase();
+    if (language.length > 2) {
+      language = language.substring(0, 2);
+    }
+    if (['de', 'fr', 'it', 'en'].indexOf(language) === -1) {
+      return 'de';
+    }
+    return language;
   }
 
   private getDeviceLanguage(): string {
@@ -42,7 +57,7 @@ class LanguageManager {
         ? NativeModules.SettingsManager.settings.AppleLocale ||
           NativeModules.SettingsManager.settings.AppleLanguages[0] // iOS 13
         : NativeModules.I18nManager.localeIdentifier;
-    return deviceLanguage;
+    return this.sanitizeLanguage(deviceLanguage);
   }
 }
 
