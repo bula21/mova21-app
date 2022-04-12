0859202f12802b08e1c38fccdee646b2e78048ef
Demian Holderegger <demian.holderegger@gmail.com>
2022-04-12 23:05:25 +0200

    show location when starting an emergency call

--- src/components/infos/pages/EmergencyPage.tsx
+++ src/components/infos/pages/EmergencyPage.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
 import MovaHeadingText from '../../generic/MovaHeadingText';
-import {TouchableOpacity} from 'react-native';
+import {PermissionsAndroid, TouchableOpacity} from 'react-native';
 import IconBack from '../../generic/IconBack';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {IPage} from '../IPage';
@@ -11,7 +11,8 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import MovaTheme from "../../../constants/MovaTheme";
 import MovaText from "../../generic/MovaText";
 import PageRefreshScrollView from "../PageRefreshScrollView";
-import Geolocation from 'react-native-geolocation-service';
+import Geolocation, {GeoPosition} from 'react-native-geolocation-service';
+import {useTranslation} from 'react-i18next';
 
 const PageContainer = styled.SafeAreaView`
   background-color: #fff;
@@ -48,9 +49,51 @@ type Props = { navigation: StackNavigationProp<RootStackParamList, 'infospage'>;
 
 export default function GenericPage({navigation, page}: Props) {
 
-  const initiateCall = () => {
-    console.log('Send location here...');
-    Linking.openURL(`tel:${page.data.phone_number}`)
+  const {t} = useTranslation();
+
+  const [location, setLocation] = useState<GeoPosition|null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [locationErrorMessage, setLocationErrorMessage] = useState<string>('');
+
+  const initiateCall = async () => {
+    if (isLoading) {
+      return; // only call once
+    }
+    setIsLoading(true);
+    setLocation(null);
+    setLocationErrorMessage('');
+
+    try {
+      // wait for user permissions and ignore the result
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "Standort senden",
+          message: "Deinen Standort an die mova Notfall-Zentrale übermitteln.",
+          buttonNeutral: "Jetzt nicht",
+          buttonNegative: "Abbrechen",
+          buttonPositive: "OK"
+        }
+      );
+    } catch (err) {
+      // ignore errors
+      console.warn(err);
+    } finally {
+      Geolocation.getCurrentPosition(
+        (position) => {
+          setLocation(position);
+          setIsLoading(false);
+          Linking.openURL(`tel:${page.data.phone_number}`)
+        },
+        (error) => {
+          console.log(error.code, error.message);
+          setIsLoading(false);
+          setLocationErrorMessage(error.message);
+          Linking.openURL(`tel:${page.data.phone_number}`)
+        },
+        {enableHighAccuracy: true, timeout: 3000, maximumAge: 60000}
+      );
+    }
   }
   return (
     <PageRefreshScrollView>
@@ -68,10 +111,12 @@ export default function GenericPage({navigation, page}: Props) {
             <TouchableOpacity onPress={() => initiateCall()}>
               <CallButtonContent>
                 <Icon name="phone" size={40} color="black" />
-                <MovaText style={{fontSize: 24, paddingLeft: 20}}>{page.data.phone_number}</MovaText>
+                <MovaText style={{fontSize: 24, paddingLeft: 20}}>{isLoading ? t('starting_call') + ' ...' : page.data.phone_number}</MovaText>
               </CallButtonContent>
             </TouchableOpacity>
           </CallContainer>
+          {location ? <MovaText style={{fontSize: 10, color: '#ccc', textAlign: 'center'}}>{t('location')}: {location.coords.latitude}, {location.coords.longitude}, {t('location_accuracy')}: {Math.round(location.coords.accuracy)}m</MovaText> : null}
+          {locationErrorMessage ? <MovaText style={{fontSize: 10, color: '#c51919', textAlign: 'center'}}>{t('location')}: {locationErrorMessage}</MovaText> : null}
           <MovaMarkdown>{page.content}</MovaMarkdown>
         </PageContent>
       </PageContainer>
