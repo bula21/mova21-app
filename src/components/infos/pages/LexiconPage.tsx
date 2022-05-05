5322390e376922761c502de675d1c400ce255009
samuel0007 <russo.samuel20@gmail.com>
2022-05-05 14:01:02 +0200

    feat: create generic searchbar component

--- src/components/infos/pages/LexiconPage.tsx
+++ src/components/infos/pages/LexiconPage.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components/native';
-import MovaHeadingText from '../../generic/MovaHeadingText';
 import {Platform, SectionList, TouchableOpacity, View} from 'react-native';
-import IconBack from '../../generic/IconBack';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {IPage} from '../IPage';
 import MovaTheme from '../../../constants/MovaTheme';
 import untypedWORDS from '../../../../assets/lexicon.json';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 import languageManager from '../../../helpers/LanguageManager';
 import MovaAccordion from '../../generic/MovaAccordion';
 import MovaText from '../../generic/MovaText';
-
+import MovaSearchbarHeading from '../../generic/MovaSearchbarHeading';
 
 interface IWord {
-  "en": string;
-  "de": string;
-  "fr": string;
-  "it": string;
+  en: string;
+  de: string;
+  fr: string;
+  it: string;
+}
+
+interface ISection {
+  title: string;
+  data: IWord[];
+  elementsCount: number;
 }
 
 const WORDS: IWord[] = untypedWORDS;
@@ -28,13 +32,10 @@ const PageContainer = styled.SafeAreaView`
 `;
 
 const PageHeader = styled.View`
-  padding: 10px;
-  margin-top: 10px;
   background: ${MovaTheme.colorYellow};
+  height: 80px;
 `;
-const PageContent = styled.View`
-`;
-
+const PageContent = styled.View``;
 
 const SectionHeaderText = styled.Text`
   padding: 10px;
@@ -43,75 +44,81 @@ const SectionHeaderText = styled.Text`
   font-family: ${Platform.OS === 'ios' ? 'MessinaSans-Bold' : 'MS-Bold'};
 `;
 
-
 type RootStackParamList = {infospage: {page: IPage}};
-type Props = { navigation: StackNavigationProp<RootStackParamList, 'infospage'>; page: IPage; };
+type Props = {navigation: StackNavigationProp<RootStackParamList, 'infospage'>; page: IPage};
 
 export default function LexiconPage({navigation, page}: Props) {
   const {t} = useTranslation();
 
   const otherLanguagesIndex = ['de', 'en', 'fr', 'it'].filter(lang => lang !== languageManager.currentLanguage);
   const currentLanguageIndex = languageManager.currentLanguage as keyof IWord;
+  const [sections, setSections] = useState<ISection[]>([]);
 
-  // Sort data by current language indexing
-  const sortedWords = WORDS.sort((a, b) => {
-    const aValue = a[currentLanguageIndex];
-    const bValue = b[currentLanguageIndex];
-    if (aValue < bValue) {
-      return -1;
-    }
-    if (aValue > bValue) {
-      return 1;
-    }
-    return 0;
-  });
+  const createSectionsFromWords = (words: IWord[]) => {
+    const sortedWords = words.sort((a, b) => {
+      const aValue = a[currentLanguageIndex];
+      const bValue = b[currentLanguageIndex];
+      if (aValue < bValue) {
+        return -1;
+      }
+      if (aValue > bValue) {
+        return 1;
+      }
+      return 0;
+    });
 
-  // Create a list of sections based on the first letter of the sorted words
-  const sections = sortedWords.reduce((sections: any[], word: IWord) => {
-    const firstLetter = word[currentLanguageIndex].charAt(0).toUpperCase();
-    const section = sections.find(section => section.title === firstLetter);
-    if (section) {
-      section.data.push(word);
-      section.elementsCount += 1;
-    } else {
-      sections.push({title: firstLetter, data: [word], elementsCount: 1});
-    }
-    
-    return sections;
+    const sections = sortedWords.reduce((sections: ISection[], word: IWord) => {
+      const firstLetter = word[currentLanguageIndex].charAt(0).toUpperCase();
+      const section = sections.find(section => section.title === firstLetter);
+      if (section) {
+        section.data.push(word);
+        section.elementsCount += 1;
+      } else {
+        sections.push({title: firstLetter, data: [word], elementsCount: 1});
+      }
+      return sections;
+    }, []);
+    setSections(sections);
+  };
+
+  const handleSearch = (words: IWord[]) => {
+    console.log('Searching for: ', words);
+    createSectionsFromWords(words);
+  };
+
+  // Initial sections load
+  useEffect(() => {
+    createSectionsFromWords(WORDS);
+    console.log(page.title);
   }, []);
 
-
   return (
-      <PageContainer>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <PageHeader>
-            <MovaHeadingText>
-              <IconBack /> {page.title}
-            </MovaHeadingText>
-          </PageHeader>
-        </TouchableOpacity>
-        <PageContent>
+    <PageContainer>
+      <PageHeader>
+        {/* <IconBack /> {page.title} */}
+        <MovaSearchbarHeading
+          headerText={page.title}
+          searchableAttributes={['en', 'de', 'fr', 'it']}
+          getData={() => WORDS}
+          handleSearch={handleSearch}
+          navigation={navigation}></MovaSearchbarHeading>
+      </PageHeader>
+      <PageContent>
         <SectionList
           sections={sections}
           renderItem={({item, index, section}) => (
-            <View style={{borderBottomWidth: index == section.elementsCount-1 ? 0 : 3}}>
+            <View style={{borderBottomWidth: index == section.elementsCount - 1 ? 0 : 3}}>
               <MovaAccordion header={item[currentLanguageIndex]}>
-                {
-                  otherLanguagesIndex.map(lang => (
-                    <MovaText key={lang}>{item[lang as keyof IWord]}</MovaText>
-                  ))
-                }
+                {otherLanguagesIndex.map(lang => (
+                  <MovaText key={lang}>{item[lang as keyof IWord]}</MovaText>
+                ))}
               </MovaAccordion>
             </View>
-            )
-          }
-          renderSectionHeader={({ section: { title } }) => (
-            <SectionHeaderText>{title}</SectionHeaderText>
           )}
+          renderSectionHeader={({section: {title}}) => <SectionHeaderText>{title}</SectionHeaderText>}
           keyExtractor={(item, index) => index.toString()}
         />
-
-        </PageContent>
-      </PageContainer>
+      </PageContent>
+    </PageContainer>
   );
 }
