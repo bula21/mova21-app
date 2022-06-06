bula21/mova21-app

commit 7fca981f86509596e77d1e5a017a5403f888d9b9

Feature/lexicon (#71)
* feat: basic lexicon renderer

* feat: create generic searchbar component

* Implement generic searchbar to infosList

* change accordion icons

* make whole area clickable (like on the other pages)

Co-authored-by: Demian Holderegger <demian.holderegger@gmail.com>

src/components/generic/MovaSearchbarHeading.tsx
@@ -1,0 +1,171 @@
+import React, {useRef, useState} from 'react';
+import {Animated, Easing, Platform, TouchableOpacity} from 'react-native';
+import styled from 'styled-components/native';
+import MovaHeadingText from './MovaHeadingText';
+import {useTranslation} from 'react-i18next';
+import MovaIcon from './MovaIcon';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {IPage} from '../infos/IPage';
+import IconBack from './IconBack';
+
+type RootStackParamList = {infospage: {page: IPage}};
+
+type Props<T> = {
+  headerText: string;
+  searchableAttributes: (keyof T)[];
+  getData: () => T[];
+  handleSearch: (data: T[]) => void;
+  navigation?: StackNavigationProp<RootStackParamList, 'infospage'>;
+  isFocused?: boolean;
+};
+
+const SearchBarHeader = styled.View`
+  flex: 1;
+  padding: 10px;
+  flex-direction: row;
+`;
+
+const SearchBar = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SearchInput = styled.TextInput`
+  font-size: 32px;
+  border-bottom-width: 3px;
+  border-color: black;
+  margin-top: -10px;
+  margin-left: 10px;
+  flex-grow: 1;
+`;
+
+const MovaHeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const MovaAnimatedHeaderContent = Animated.createAnimatedComponent(MovaHeaderContent);
+
+export default function MovaSearchbarHeading<T>(props: Props<T>) {
+  const {t} = useTranslation();
+  const [isSearchActive, setSearching] = useState<boolean>(false);
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const animationProgress = useRef(new Animated.Value(0)).current;
+
+  if(props.isFocused) {
+    if (isSearchActive && !props.isFocused) {
+      leaveSearch();
+    }
+  }
+  
+  function enterSearch() {
+    Animated.timing(animationProgress, {
+      toValue: 1,
+      duration: 400,
+      useNativeDriver: false,
+      easing: Easing.out(Easing.circle),
+    }).start(() => {
+      setSearching(true);
+      animationProgress.setValue(0);
+    });
+  }
+
+  function clearSearch() {
+    if (searchKeyword === '') {
+      leaveSearch();
+    } else {
+      onNewSearchKeyword('');
+    }
+  }
+
+  function leaveSearch() {
+    onNewSearchKeyword('');
+    setSearching(false);
+    animationProgress.setValue(1);
+    Animated.timing(animationProgress, {
+      toValue: 0,
+      duration: 400,
+      useNativeDriver: false,
+      easing: Easing.out(Easing.circle),
+    }).start();
+  }
+
+  function onNewSearchKeyword(keyword: string) {
+    setSearchKeyword(keyword);
+    let objects = props.getData();
+    if (keyword === '') {
+      props.handleSearch(objects);
+      return;
+    }
+    if (isSearchActive && typeof keyword != 'undefined' && keyword) {
+      const sanitized = keyword.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+      const regexp = new RegExp(sanitized, 'i');
+      objects = objects.filter(object => {
+        for (const key of props.searchableAttributes) {
+          let str = object[key];
+          if (typeof str === 'string' && regexp.test(str)) {
+            return true;
+          }
+        }
+      });
+    }
+    props.handleSearch(objects);
+  }
+
+  return (
+    <SearchBarHeader>
+      {!isSearchActive && (
+        <MovaAnimatedHeaderContent
+          style={[
+            {
+              flex: animationProgress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 0.00001],
+              }),
+              opacity: animationProgress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 0.5],
+              }),
+              fontSize: animationProgress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [40, 1],
+              }),
+              marginTop: animationProgress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, 25],
+              }),
+            },
+          ]}>
+          {props.navigation ? (
+            <TouchableOpacity style={{marginRight: 12}} onPress={() => props.navigation!.goBack()}>
+              <MovaHeadingText style={{marginTop: 10}}>
+                <IconBack /> {props.headerText}
+              </MovaHeadingText>
+            </TouchableOpacity>
+          ) : <MovaHeadingText style={{marginTop: -5}}>{props.headerText}</MovaHeadingText>}
+        </MovaAnimatedHeaderContent>
+      )}
+      <SearchBar style={isSearchActive ? {flexGrow: 1} : {}}>
+        <MovaIcon
+          name="search-outline"
+          size={50}
+          color="black"
+          onPress={() => (isSearchActive ? leaveSearch() : enterSearch())}
+        />
+        {isSearchActive && (
+          <SearchInput onChangeText={onNewSearchKeyword} value={searchKeyword} placeholder={t('search_keyword')} />
+        )}
+        {isSearchActive && (
+          <Icon
+            name={'close-sharp'}
+            size={40}
+            color="black"
+            onPress={() => clearSearch()}
+            style={{paddingLeft: 10}}
+          />
+        )}
+      </SearchBar>
+    </SearchBarHeader>
+  );
+}

src/components/infos/InfosList.tsx
@@ -1,17 +1,15 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components/native';
-import MovaHeadingText from '../generic/MovaHeadingText';
-import {Animated, Easing, FlatList, RefreshControl, TouchableOpacity} from 'react-native';
+import {FlatList, RefreshControl, TouchableOpacity} from 'react-native';
 import {useTranslation} from 'react-i18next';
 import MovaTheme from '../../constants/MovaTheme';
 import MovaText from '../generic/MovaText';
-import MovaIcon from '../generic/MovaIcon';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {IPage} from './IPage';
-import {InfopagesStore} from "../../stores/InfopagesStore";
-import LanguageSwitcher from "./LanguageSwitcher";
-import { useIsFocused } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {InfopagesStore} from '../../stores/InfopagesStore';
+import LanguageSwitcher from './LanguageSwitcher';
+import {useIsFocused} from '@react-navigation/native';
+import MovaSearchbarHeading from '../generic/MovaSearchbarHeading';
 
 const MainContainer = styled.SafeAreaView`
   background-color: #fff;
@@ -23,41 +21,16 @@ const InfosItem = styled.View`
 `;
 
 const InfosHeader = styled.View`
-  padding: 10px;
-  margin-top: 10px;
-  flex-direction: row;
+  height: 80px;
 `;
 
-const SearchBar = styled.View`
-  flex-direction: row;
-  height: 56px;
-  align-content: center;
-  align-items: center;
-  justify-content: space-between;
-`;
+type NavigationProp = StackNavigationProp<{infopage: {page: IPage}}, 'infopage'>;
 
-const SearchInput = styled.TextInput`
-  font-size: 32px;
-  border-bottom-width: 3px;
-  border-color: black;
-  margin-top: -10px;
-  margin-left: 10px;
-  height: 60px;
-  flex-grow: 1;
-`;
-
-const MovaAnimatedHeadingText = Animated.createAnimatedComponent(MovaHeadingText);
-
-type NavigationProp = StackNavigationProp<
-  {infopage: {page: IPage}},
-  'infopage'
->;
-
-function mainPageFilter(page: IPage) : boolean {
+function mainPageFilter(page: IPage): boolean {
   return page.sub_page === false;
 }
 
-function mainPages() : IPage[] {
+function mainPages(): IPage[] {
   return InfopagesStore.get().filter(mainPageFilter);
 }
 
@@ -67,28 +40,22 @@ export default function InfosList({navigation}: {navigation: NavigationProp}) {
   const [pages, setPages] = useState<IPage[]>([]);
   const [isRefreshing, setRefreshing] = useState<boolean>(false);
 
-  const [isSearchActive, setSearching] = useState<boolean>(false);
-  const [searchKeyword, setSearchKeyword] = useState("");
-
-  const animationProgress = useRef(new Animated.Value(0)).current;
+  const handleSearch = (pages: IPage[]) => {
+    setPages(pages);
+  };
+  const isFocused = useIsFocused();
 
   // load on mount
   useEffect(() => {
     setPages(mainPages());
     const subscription = InfopagesStore.subscribe((pages: IPage[]) => {
       setPages(pages.filter(mainPageFilter));
-    })
+    });
     InfopagesStore.reload();
     return () => {
       subscription.unsubscribe();
     };
   }, []);
-
-  const isFocused = useIsFocused();
-
-  if (isSearchActive && !isFocused) {
-    leaveSearch();
-  }
 
   function onRefresh() {
     setRefreshing(true);
@@ -105,134 +72,33 @@ export default function InfosList({navigation}: {navigation: NavigationProp}) {
     return index % 2 ? MovaTheme.colorBlue : MovaTheme.colorYellow;
   }
 
-  function enterSearch() {
-    Animated.timing(animationProgress, {
-      toValue: 1,
-      duration: 400,
-      useNativeDriver: false,
-      easing: Easing.out(Easing.circle),
-    }).start(
-      () => {
-        setSearching(true);
-        animationProgress.setValue(0);
-      }
-    );
-  };
-
-  function clearSearch() {
-    if (searchKeyword === '') {
-      leaveSearch();
-    } else {
-      onNewSearchKeyword('');
-    }
-  }
-
-  function leaveSearch() {
-    onNewSearchKeyword('');
-    setSearching(false);
-    animationProgress.setValue(1);
-    Animated.timing(animationProgress, {
-      toValue: 0,
-      duration: 400,
-      useNativeDriver: false,
-      easing: Easing.out(Easing.circle),
-    }).start();
-  };
-
-  function onNewSearchKeyword(keyword: string) {
-    setSearchKeyword(keyword);
-    if (keyword === '') {
-      setPages(mainPages());
-      return;
-    }
-    // Search shows all pages, including sub pages
-    let pages = InfopagesStore.get();
-    if (isSearchActive && typeof keyword != 'undefined' && keyword) {
-      const sanitized = keyword.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-      const regexp = new RegExp(sanitized, 'i');
-      pages = pages.filter((page) =>
-        regexp.test(page.title) || regexp.test(page.content)
-      );
-    }
-    setPages(pages);
-  }
-
   return (
     <MainContainer>
       <FlatList
         data={pages}
-        renderItem={({ item, index }) => (
-          <TouchableOpacity
-            onPress={() => navigation.navigate('infopage', { page: item })}>
+        renderItem={({item, index}) => (
+          <TouchableOpacity onPress={() => navigation.navigate('infopage', {page: item})}>
             <InfosItem
               style={{
-                backgroundColor: getColor(item, index)
+                backgroundColor: getColor(item, index),
               }}>
-              <MovaText style={{ fontSize: 40 }}>{item.title}</MovaText>
+              <MovaText style={{fontSize: 40}}>{item.title}</MovaText>
             </InfosItem>
           </TouchableOpacity>
         )}
-        keyExtractor={(item) => String(item.id)}
+        keyExtractor={item => String(item.id)}
         ListHeaderComponent={
           <InfosHeader>
-            {!isSearchActive &&
-              <MovaAnimatedHeadingText
-                style={[MovaHeadingText.style, {
-                  flex: animationProgress.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [1, 0.00001]
-                  }),
-                  opacity: animationProgress.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [1, 0.5]
-                  }),
-                  fontSize: animationProgress.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [40, 1]
-                  }),
-                  marginTop: animationProgress.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0, 25]
-                  }),
-                }]}
-              >
-                {t('info')}
-              </MovaAnimatedHeadingText>
-            }
-            <SearchBar
-              style={isSearchActive ? { flexGrow: 1 } : {}}
-            >
-              <MovaIcon
-                name='search-outline'
-                size={50}
-                color='black'
-                onPress={() => isSearchActive ? leaveSearch() : enterSearch()}
-              />
-              {isSearchActive &&
-                <SearchInput
-                  onChangeText={onNewSearchKeyword}
-                  value={searchKeyword}
-                  placeholder={t('search_keyword')}
-                />
-              }
-              {isSearchActive &&
-                <Icon
-                  name={'close-sharp'}
-                  size={40}
-                  color='black'
-                  onPress={() => clearSearch()}
-                  style={{ paddingBottom: 12, paddingLeft: 10 }}
-                />
-              }
-            </SearchBar>
+            <MovaSearchbarHeading
+              headerText={t('info')}
+              searchableAttributes={['title', 'content']}
+              getData={() => InfopagesStore.get()}
+              handleSearch={handleSearch}
+              isFocused={isFocused}></MovaSearchbarHeading>
           </InfosHeader>
         }
-        ListFooterComponent={
-          <LanguageSwitcher />
-        }
-        refreshControl={
-          <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
-        }
+        ListFooterComponent={<LanguageSwitcher />}
+        refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />}
       />
     </MainContainer>
   );

src/components/infos/pages/LexiconPage.tsx
@@ -1,0 +1,124 @@
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components/native';
+import {Platform, SectionList, TouchableOpacity, View} from 'react-native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {IPage} from '../IPage';
+import MovaTheme from '../../../constants/MovaTheme';
+import untypedWORDS from '../../../../assets/lexicon.json';
+import {useTranslation} from 'react-i18next';
+import languageManager from '../../../helpers/LanguageManager';
+import MovaAccordion from '../../generic/MovaAccordion';
+import MovaText from '../../generic/MovaText';
+import MovaSearchbarHeading from '../../generic/MovaSearchbarHeading';
+
+interface IWord {
+  en: string;
+  de: string;
+  fr: string;
+  it: string;
+}
+
+interface ISection {
+  title: string;
+  data: IWord[];
+  elementsCount: number;
+}
+
+const WORDS: IWord[] = untypedWORDS;
+
+const PageContainer = styled.SafeAreaView`
+  background-color: #fff;
+  flex: 1;
+`;
+
+const PageHeader = styled.View`
+  background: ${MovaTheme.colorYellow};
+  height: 80px;
+`;
+const PageContent = styled.View``;
+
+const SectionHeaderText = styled.Text`
+  padding: 10px;
+  font-size: 32px;
+  font-weight: bold;
+  font-family: ${Platform.OS === 'ios' ? 'MessinaSans-Bold' : 'MS-Bold'};
+`;
+
+type RootStackParamList = {infospage: {page: IPage}};
+type Props = {navigation: StackNavigationProp<RootStackParamList, 'infospage'>; page: IPage};
+
+export default function LexiconPage({navigation, page}: Props) {
+  const {t} = useTranslation();
+
+  const otherLanguagesIndex = ['de', 'en', 'fr', 'it'].filter(lang => lang !== languageManager.currentLanguage);
+  const currentLanguageIndex = languageManager.currentLanguage as keyof IWord;
+  const [sections, setSections] = useState<ISection[]>([]);
+
+  const createSectionsFromWords = (words: IWord[]) => {
+    const sortedWords = words.sort((a, b) => {
+      const aValue = a[currentLanguageIndex];
+      const bValue = b[currentLanguageIndex];
+      if (aValue < bValue) {
+        return -1;
+      }
+      if (aValue > bValue) {
+        return 1;
+      }
+      return 0;
+    });
+
+    const sections = sortedWords.reduce((sections: ISection[], word: IWord) => {
+      const firstLetter = word[currentLanguageIndex].charAt(0).toUpperCase();
+      const section = sections.find(section => section.title === firstLetter);
+      if (section) {
+        section.data.push(word);
+        section.elementsCount += 1;
+      } else {
+        sections.push({title: firstLetter, data: [word], elementsCount: 1});
+      }
+      return sections;
+    }, []);
+    setSections(sections);
+  };
+
+  const handleSearch = (words: IWord[]) => {
+    console.log('Searching for: ', words);
+    createSectionsFromWords(words);
+  };
+
+  // Initial sections load
+  useEffect(() => {
+    createSectionsFromWords(WORDS);
+    console.log(page.title);
+  }, []);
+
+  return (
+    <PageContainer>
+      <PageHeader>
+        {/* <IconBack /> {page.title} */}
+        <MovaSearchbarHeading
+          headerText={page.title}
+          searchableAttributes={['en', 'de', 'fr', 'it']}
+          getData={() => WORDS}
+          handleSearch={handleSearch}
+          navigation={navigation}></MovaSearchbarHeading>
+      </PageHeader>
+      <PageContent>
+        <SectionList
+          sections={sections}
+          renderItem={({item, index, section}) => (
+            <View style={{borderBottomWidth: index == section.elementsCount - 1 ? 0 : 3}}>
+              <MovaAccordion header={item[currentLanguageIndex]}>
+                {otherLanguagesIndex.map(lang => (
+                  <MovaText key={lang}>{item[lang as keyof IWord]}</MovaText>
+                ))}
+              </MovaAccordion>
+            </View>
+          )}
+          renderSectionHeader={({section: {title}}) => <SectionHeaderText>{title}</SectionHeaderText>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </PageContent>
+    </PageContainer>
+  );
+}

src/components/generic/MovaAccordion.tsx
@@ -1,0 +1,56 @@
+import React, { Component, useState } from 'react';
+import { TouchableOpacity, View, LayoutAnimation, Platform, UIManager} from 'react-native';
+import MovaTheme from '../../constants/MovaTheme';
+import styled from 'styled-components/native';
+import MovaIcon from './MovaIcon';
+
+type Props = {
+    header: string;
+    children: React.ReactNode;
+};
+
+const AccordionContainer = styled.View`
+    padding: 10px;
+    background-color: ${MovaTheme.colorOrange};
+`;
+
+const AccordionHeader = styled.View`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const AccordionHeaderText = styled.Text`
+    font-family: ${Platform.OS === 'ios' ? 'MessinaSans-Bold' : 'MS-Bold'};
+    font-size: 32px;
+`;
+
+
+export default function MovaAccordion(props:Props) {
+    if (Platform.OS === 'android') {
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+    const [expanded, setExpanded] = useState(false);
+    const onHeaderPress = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        setExpanded(!expanded);
+    };
+    return (
+        <AccordionContainer>
+            <TouchableOpacity onPress={onHeaderPress}>
+                <AccordionHeader>
+                    <AccordionHeaderText>{props.header}</AccordionHeaderText>
+                    <MovaIcon size={40} name={expanded ? 'minus' : 'plus'} />
+                </AccordionHeader>
+            </TouchableOpacity>
+            <View/>
+            {
+                expanded &&
+                <View>
+                    {props.children}
+                </View>
+            }
+       </AccordionContainer>
+        );
+}

src/components/infos/InfosPage.tsx
@@ -5,6 +5,7 @@ import GenericPage from './pages/GenericPage';
 import BikePage from './pages/BikePage';
 import EmergencyPage from './pages/EmergencyPage';
 import SponsorsPage from './pages/SponsorsPage';
+import LexiconPage from './pages/LexiconPage';
 import {InfopagesStore} from '../../stores/InfopagesStore';
 
 type RootStackParamList = {infospage: {page: IPage}};
@@ -34,6 +35,7 @@ export default function InfosPage({route, navigation}: Props) {
     case 'bike': return <BikePage navigation={navigation} page={page} />;
     case 'emergency': return <EmergencyPage navigation={navigation} page={page} />;
     case 'sponsors': return <SponsorsPage navigation={navigation} page={page} />;
+    case 'lexicon': return <LexiconPage navigation={navigation} page={page} />;
     default: return <GenericPage navigation={navigation} page={page} />;
   }
 }

commit 72dc0d5549ddad6af43ca57969d2acf485af6310

Fix subpages not removed after search resets
Fixes a regression introduced in #71.

src/components/generic/MovaSearchbarHeading.tsx
@@ -14,7 +14,10 @@ type RootStackParamList = {infospage: {page: IPage}};
 type Props<T> = {
   headerText: string;
   searchableAttributes: (keyof T)[];
+  /** All data for which data should be applied. */
   getData: () => T[];
+  /** Default data is displayed when search is inactive. */
+  getDefaultData: () => T[];
   handleSearch: (data: T[]) => void;
   navigation?: StackNavigationProp<RootStackParamList, 'infospage'>;
   isFocused?: boolean;
@@ -93,11 +96,11 @@ export default function MovaSearchbarHeading<T>(props: Props<T>) {
 
   function onNewSearchKeyword(keyword: string) {
     setSearchKeyword(keyword);
-    let objects = props.getData();
     if (keyword === '') {
-      props.handleSearch(objects);
+      props.handleSearch(props.getDefaultData());
       return;
     }
+    let objects = props.getData();
     if (isSearchActive && typeof keyword != 'undefined' && keyword) {
       const sanitized = keyword.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
       const regexp = new RegExp(sanitized, 'i');

src/components/infos/InfosList.tsx
@@ -100,7 +100,8 @@ export default function InfosList({navigation}: {navigation: NavigationProp}) {
                   <MovaSearchbarHeading
                     headerText={t('info')}
                     searchableAttributes={['title', 'content']}
-                    getData={() => InfopagesStore.get()}
+                    getData={InfopagesStore.get}
+                    getDefaultData={mainPages}
                     handleSearch={handleSearch}
                     isFocused={isFocused}></MovaSearchbarHeading>
                 </InfosHeader>

src/components/infos/pages/LexiconPage.tsx
@@ -100,6 +100,7 @@ export default function LexiconPage({navigation, page}: Props) {
           headerText={page.title}
           searchableAttributes={['en', 'de', 'fr', 'it']}
           getData={() => WORDS}
+          getDefaultData={() => WORDS}
           handleSearch={handleSearch}
           navigation={navigation}></MovaSearchbarHeading>
       </PageHeader>