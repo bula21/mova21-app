2af24e7fcf54c7074eee831c8e1e4a26f660f68e
Demian Holderegger <demian.holderegger@gmail.com>
2022-06-08 23:42:01 +0200

    small news images fix. closes #90

--- tools/news-sync/cron.php
+++ tools/news-sync/cron.php
@@ -27,6 +27,8 @@ function http_get_json($url) {
 }
 
 function html2markdown($html) {
+	// fix images without src and srcset only
+	$html = preg_replace('/<img srcset="(([^ ]*) )([^"]*)"/i', '<img src="$2"', $html);
 	$converter = new HtmlConverter();
 	$converter->getConfig()->setOption('strip_tags', true);
 	return $converter->convert($html);
