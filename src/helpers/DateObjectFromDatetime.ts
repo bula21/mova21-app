9e50ad749679709f399c3345386b9c9bbde430e0
Demian Holderegger <demian.holderegger@gmail.com>
2022-07-10 21:57:26 +0200

    fix news date timezone. closes #110

--- src/helpers/DateObjectFromDatetime.ts
+++ src/helpers/DateObjectFromDatetime.ts
@@ -6,14 +6,12 @@ export default function DateObjectFromDatetime(datetime: string | null) {
     // Apply each element to the Date function
     if (t.length >= 6) {
       return new Date(
-        Date.UTC(
           parseInt(t[0]),
           parseInt(t[1]) - 1,
           parseInt(t[2]),
           parseInt(t[3]),
           parseInt(t[4]),
           parseInt(t[5]),
-        ),
       );
     }
   }
